dbe6534c796e61879227e906f33b1182a724e046
RameshAdhikari860 <ourworld898@gmail.com>
2025-08-13 09:14:18 +0545

    get admin product and delte

--- src/handleOperation/apiLinks.js
+++ src/handleOperation/apiLinks.js
@@ -2,5 +2,6 @@ const BASE_URL = 'http://localhost:4000/api'
 
 export const apiLinks = {
     getAllProduct: `${BASE_URL}/product/getAllProduct`,
+    deleteProduct : (id)=>(`${BASE_URL}/product/deleteProductById/${id}`)
 
 }
--- src/handleOperation/handleOperation.js
+++ src/handleOperation/handleOperation.js
@@ -1,20 +1,23 @@
 import axios from "axios"
 
 
-
-
- const  handleGetOperation = async (url)=>{
+export  const  handleGetOperation = async (url)=>{
     const result =    await  axios.get(url,{withCredentials:true})
-    console.log(result)
-   return result.data
+
+   return result
 
 }
 
-
-const handlePostOperation = async (url,data)=>{
+export const handlePostOperation = async (url,data)=>{
     const result = await axios.post(url,data,{withCredentials:true})
     return result.data;
 }
 
-export {handleGetOperation}
+
+export const handleDeleteOperation = async (url)=>{
+
+    const result = await axios.delete(url)
+    return result
+
+}
 
